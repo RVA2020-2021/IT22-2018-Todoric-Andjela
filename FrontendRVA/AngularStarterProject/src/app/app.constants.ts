79576089721f6317fab91f3f8bae51a40ce38c85
andjelatodoric <andjelatodoric@uns.ac.rs>
2021-06-09 11:50:24 +0200

    Test 2

--- FrontendRVA/AngularStarterProject/src/app/app.constants.ts
+++ FrontendRVA/AngularStarterProject/src/app/app.constants.ts
@@ -1,5 +1,12 @@
-export const LIGA_URL = 'http://localhost:8083/liga';
+/*export const LIGA_URL = 'http://localhost:8083/liga';
 export const NACIONALNOST_URL = 'http://localhost:8083/nacionalnost';
 export const TIM_URL = 'http://localhost:8083/tim';
 export const IGRACI_TIMA_URL = 'http://localhost:8083/igraciTim';
-export const IGRAC_URL = 'http://localhost:8083/igrac';
+export const IGRAC_URL = 'http://localhost:8083/igrac';*/
+
+export const LIGA_URL = 'https://backend-rva-andjela.herokuapp.com/liga';
+export const NACIONALNOST_URL = 'https://backend-rva-andjela.herokuapp.com/nacionalnost';
+export const TIM_URL = 'https://backend-rva-andjela.herokuapp.com/tim';
+export const IGRACI_TIMA_URL = 'https://backend-rva-andjela.herokuapp.com/igraciTim';
+export const IGRAC_URL = 'https://backend-rva-andjela.herokuapp.com/igrac';
+
